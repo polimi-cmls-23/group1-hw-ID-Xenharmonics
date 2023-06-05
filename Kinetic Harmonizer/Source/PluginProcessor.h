5fcdf73f6e700c124e1a1e429ae3cdbf42475d91
aleferrando <101126852+aleferrando@users.noreply.github.com>
2023-06-05 06:28:00 +0200

    Update PluginProcessor.h

--- Kinetic Harmonizer/Source/PluginProcessor.h
+++ Kinetic Harmonizer/Source/PluginProcessor.h
@@ -25,6 +25,11 @@ public:
     void releaseResources() override;
 
     void oscMessageReceived(const OSCMessage& message) override;
+    
+    float mapf(float x, float in_min, float in_max, float out_min, float out_max)
+    {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
 
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
